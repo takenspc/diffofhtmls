cecb5f9af6ec2d44ff15b51551d753eda26ac6bc
Takeshi Kurosawa <taken.spc@gmail.com>
2016-02-09 23:17:22 +0900

    Use .sectionLink in the log page

--- routes/log.js
+++ routes/log.js
@@ -3,6 +3,7 @@ var path = require('path');
 var express = require('express');
 var router = express.Router();
 var utils = require('./utils/firebase');
+var links = require('./utils/links');
 
 
 router.get('/atom', function (req, res, next) {
@@ -18,7 +19,26 @@ router.get('/atom', function (req, res, next) {
 
 
 router.get('/', function (req, res, next) {
-    utils.loadUpdates().then(function (updates) {
+    Promise.all([
+        utils.loadIndexJSON(),
+        utils.loadUpdates(),
+    ]).then(function (data) {
+        var index = data[0];
+        var updates = data[1];
+        
+        // XXX 
+        for (var i = 0; i < updates.length; i++) {
+            var update = updates[i];
+            if (!update.updated) {
+                continue;
+            }
+
+            for (var j = 0; j < update.updated.length; j++) {
+                var updated = update.updated[j];
+                updated.section = links.findSection(index, updated.path);
+            }
+        }
+        
         res.render('log', {
             title: 'Update information',
             updates: updates,
