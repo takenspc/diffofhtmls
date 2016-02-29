3a82f7aca54c37a28158a5bb7f78459bc69e7f0e
Takeshi Kurosawa <taken.spc@gmail.com>
2016-02-29 21:50:13 +0900

    Tweak execution timing of highlighting

--- public/script/init.js
+++ public/script/init.js
@@ -1,8 +1,10 @@
 /* global hljs */
 (function() {
     'use strict';
-    var cells = document.querySelectorAll('.diff tbody td');
-    for (var i = 0, len = cells.length; i < len; i++) {
-         hljs.highlightBlock(cells[i]);
-    }
+    document.addEventListener('DOMContentLoaded', function() {
+        var cells = document.querySelectorAll('.diff tbody td');
+        for (var i = 0, len = cells.length; i < len; i++) {
+            hljs.highlightBlock(cells[i]);
+        }
+    });
 })();
