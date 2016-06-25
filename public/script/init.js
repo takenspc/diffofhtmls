d4d05d193c977cb8932169e8e00112409c9461b5
Takeshi Kurosawa <taken.spc@gmail.com>
2016-06-25 19:53:22 +0900

    Reduce file sizes

--- public/script/init.js
+++ public/script/init.js
@@ -1,10 +1,8 @@
 /* global hljs */
 (function() {
     'use strict';
-    document.addEventListener('DOMContentLoaded', function() {
-        var cells = document.querySelectorAll('.diff tbody td');
-        for (var i = 0, len = cells.length; i < len; i++) {
-            hljs.highlightBlock(cells[i]);
-        }
-    });
+    var cells = document.querySelectorAll('.diff tbody td');
+    for (var i = 0, len = cells.length; i < len; i++) {
+        hljs.highlightBlock(cells[i]);
+    }
 })();
